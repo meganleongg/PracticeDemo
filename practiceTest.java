183e5565f4623576f28c314df659f153b5540236
meganleongg <98578642+meganleongg@users.noreply.github.com>
2022-05-01 21:36:05 -0700

    Added Method and JUnit Test

--- practiceTest.java
+++ practiceTest.java
@@ -1,4 +1,7 @@
 public class practiceTest{
     
+   public static int subtract(int n, int m) {
+      return n-m;
+   }
    }
    
---
+++ practiceTester.java
@@ -0,0 +1,14 @@
+import java.beans.Transient;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class practiceTester {
+    @Test
+    public void subtraction() {
+        assertEquals(2, practiceTest.subtract(1,1));
+    }
+
+    
+    
+}
